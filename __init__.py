aa82d200dda8865f12811f72087f52d1d29f3d1f
Pisteur_alpin <pisteur.alpin@gmail.com>
2023-04-15 00:00:32 +0200

    Trajectories
    Add 3 trajectories:
    - Inert boat, drifting due to currents
    - Following the initial heading but drifting
    - Adapting its heading
    
    Known issue:
    - Bug in 25% of the tests

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.0.1"
+# ----------------------------------------------------------- #
+#         SELF-STEERING BOAT AND CURRENTS SIMULATION          #
+#  ©2023 Mathurin Roulier, PTSI Lycée Lesage, Vannes, France  #
+# ----------------------------------------------------------- #
+
+__version__ = "0.0.2"
